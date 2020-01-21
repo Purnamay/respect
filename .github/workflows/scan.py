7e1fc635087b4ed9ac28fdf76d153c092ba6a3e5
Purnamay <60135675+Purnamay@users.noreply.github.com>
2020-01-21 07:15:08 -0500

    respect

---
+++ .github/workflows/scan.py
@@ -0,0 +1,17 @@
+name: respect 
+
+on: [respect]
+
+jobs: for aadhar scanning and Debit/Credit Card account Information
+  build:   If yo can cath me i will be you fan forever
+
+    runs-on: ubuntu-latest
+
+    steps:   all the nation and international orgonisation
+    - uses: actions/checkout@v1
+    - name: Run on a scan.py application
+      run: echo Hello
+    - name: Run a multi-line script
+      run: |
+        echo Add other actions to build,
+        echo test, and deploy your project.
